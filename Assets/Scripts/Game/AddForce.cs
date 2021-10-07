c9fdf8ecdebfd1950b57103c64aafce81df82ef2
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-10-07 13:08:32 +0900

    Enemyのノックバック

--- Assets/Scripts/Game/AddForce.cs
+++ Assets/Scripts/Game/AddForce.cs
@@ -11,27 +11,14 @@ public class AddForce
         float dir = thisPos.position.x - otherPos.position.x;
 
         if (dir > 0)
-            angle = 45;
+            angle = Random.Range(115, 140);
 
         else if (dir < 0)
-            angle = 135;
+            angle = Random.Range(25, 50);
 
         float rad = angle * Mathf.Deg2Rad;
         Vector2 force = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
 
         targetRb.velocity = force * m_power;
-        Task.Run(() => ReverseForce(force, targetRb));
-    }
-
-    void ReverseForce(Vector2 dir, Rigidbody2D rb)
-    {
-        Vector2 updateDir = Vector2.zero;
-        for (float i = dir.x; i > 0; i -= 0.05f)
-        {
-            Debug.Log(i);
-            updateDir = new Vector2(i * -1, dir.y);
-            Debug.Log(updateDir * m_power);
-            //rb.velocity = updateDir * 50;
-        }
     }
 }
