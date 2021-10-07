528084e57c12be00c665591dc753b12915d7101d
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-10-08 01:20:34 +0900

    Enemyが死んだときの処理、過程

--- Assets/Scripts/Systems/IManager/DiedEnemy.cs
+++ Assets/Scripts/Systems/IManager/DiedEnemy.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiedEnemy : IManager
 {
+    [SerializeField] Sprite m_blood = null;
     List<Vector2> m_diedPosArray = new List<Vector2>();
 
     public void Execution()
     {
-        Debug.Log("DiedEnemy");
+        Transform playerPos = GameObject.FindGameObjectWithTag("Player").transform;
         EnemyBase[] enemies = GameObject.FindObjectsOfType<EnemyBase>();
+
         foreach (EnemyBase enemy in enemies)
         {
             if (!enemy.IsDied) continue;
             m_diedPosArray.Add(enemy.transform.position);
         }
 
-        foreach (var item in m_diedPosArray)
+        foreach (Vector2 pos in m_diedPosArray)
         {
-            Debug.Log(item);
+            SetSprite();
+            CheckDir(playerPos.position, pos);
         }
+    }
+
+    GameObject SetSprite()
+    {
+        GameObject blood = new GameObject();
+        blood.transform.localScale = new Vector2(2.5f, 2.5f);
+        SpriteRenderer sprite = blood.AddComponent<SpriteRenderer>();
+        sprite.sprite = m_blood;
+
+        return blood;
+    }
+
+    Quaternion CheckDir(Vector2 player, Vector2 target)
+    {
+        Quaternion q = Quaternion.identity;
+        float dir = target.x - player.x;
+        if (dir < 0)
+            q = Quaternion.Euler(0, 0, 0);
+        else if (dir > 0)
+            q = Quaternion.Euler(0, 180, 0);
+
+        return q;
     }
 }
