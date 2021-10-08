750e702e8d6173a506e58f0338579acda0fc5be0
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-10-09 00:47:02 +0900

    TimeRate,過程

--- Assets/Scripts/Systems/IManager/TimeRate.cs
+++ Assets/Scripts/Systems/IManager/TimeRate.cs
@@ -6,21 +6,35 @@ using System.Threading.Tasks;
 class TimeRate : IManager
 {
     [SerializeField] AnimationCurve m_curve = null;
-    public async void Execution()
-    {
-        EnemyBase enemyBase = GameObject.FindObjectOfType<EnemyBase>();
 
-        await Task.Run(() => Rate());
-        Debug.Log("End");
+    public void Execution()
+    {
+        EnemyBase[] enemyBase = GameObject.FindObjectsOfType<EnemyBase>();
+
+        GameObject set = new GameObject();
+        set.name = "RateSyastem";
+
+        RateControl rate = set.AddComponent<RateControl>();
+        rate.Target = set;
+        rate.Set(m_curve, enemyBase);
+    }
+}
+
+class RateControl : MonoBehaviour
+{
+    float m_rate = 0;
+    float m_rateSpeed = 0.05f;
+
+    public GameObject Target { private get; set; }
+
+    public void Set(AnimationCurve curve, EnemyBase[] enemy)
+    {
+        StartCoroutine(SetTime(curve, enemy));
     }
 
-    void Rate()
+    IEnumerator SetTime(AnimationCurve curve, EnemyBase[] enemy)
     {
-        float time = 0;
-        while (time >= 3)
-        {
-            time += Time.deltaTime;
-            Debug.Log(time);
-        }
+        yield return new WaitForSeconds(2f);
+        Destroy(Target);
     }
 }
